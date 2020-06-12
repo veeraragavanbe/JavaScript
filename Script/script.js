1ccf1d4163544e47a2d22b2593fc337cc0d84b7d
veera <veeraragavanbe@gmail.com>
2020-06-12 13:35:43 +0530

    datatypes

---
+++ Script/script.js
@@ -0,0 +1,13 @@
+var tree = 'Hello this is tree';
+var num1 = 21;
+var num2 = 2.1;
+var nul = null;
+var number = 21;
+var mx = Number.MAX_VALUE;
+var char = "empire"
+console.log(tree);
+console.log(num1);
+console.log(num2);
+console.log(typeof(char))
+console.log(typeof(nul))
+console.log(typeof(mx))
--- script.js
+++
@@ -1,2 +0,0 @@
-alert('Alert Box');
-console.log('Demo');
