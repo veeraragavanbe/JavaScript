b0be304e40c5fa468afdd63c330a4b7a33ed26d3
veera <veeraragavanbe@gmail.com>
2020-07-04 19:42:35 +0530

    condition

--- Script/cond.js
+++ Script/cond.js
@@ -1,4 +1,15 @@
 var a = 19,
     isminor;
+console.log("1111111111111111");
 isminor = a >= 18 ? 'True' : 'false';
+console.log(a);
 console.log(isminor);
+//assignment
+
+var deposit = 5000,
+    withdraw = 2000;
+console.log(deposit)
+deposit += 30;
+console.log(deposit);
+withdraw += deposit;
+console.log(withdraw)
---
+++ Script/ifelse.js
@@ -0,0 +1,13 @@
+var a = 150;
+var b = 1000;
+var c = 1500;
+
+if (a > b) {
+    if (a > c) {
+        console.log("a is big");
+    } else {
+        console.log("c is big");
+    }
+} else {
+    console.log("b is big")
+}
