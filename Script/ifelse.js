42aa4abeb0852db69c3ff667deadb5589e7927c2
veera <veeraragavanbe@gmail.com>
2020-07-22 11:02:58 +0530

    boxes

--- Script/ifelse.js
+++ Script/ifelse.js
@@ -1,13 +1,57 @@
-var a = 150;
-var b = 1000;
-var c = 1500;
+// // var a = 150;
+// // var b = 10000;
+// // var c = 1500;
 
-if (a > b) {
-    if (a > c) {
-        console.log("a is big");
+// // if (a > b) {
+// //     console.log("a is big");
+// // } else if (b > c) {
+// //     console.log("b is big");
+// // } else {
+// //     console.log('c is big')
+// // }
+
+
+
+// // age = 15;
+// // permision = "true"
+
+// // if (age >= 18 && permision) {
+// //     console.log("allowed for ride");
+// // } else {
+// //     console.log("not allowed")
+// // }
+
+// age = 62;
+// permision = "false"
+
+// if ((age >= 18 && permision === false) || age >= 60) {
+//     console.log("ride not allowed");
+// } else {
+//     console.log("ride allowed")
+// }
+
+// var x1 = 15 / 5;
+// var x2 = 15 % 5;
+// console.log(x1, x2);
+
+// // alert("hello");
+// var s = prompt('what is your name?', 'yamini');
+// // alert(s);
+
+// var all = prompt("Enter your name", "Empire")
+// alert(all)
+
+// var a2 = confirm("yes I love yaamini")
+// alert(a2)
+
+var age = prompt("Enter Your Age")
+if (age < 18) {
+    var par = confirm("I have parents permission");
+    if (par == true) {
+        alert("You are allowed to ride")
     } else {
-        console.log("c is big");
+        alert("You are not allowed to ride")
     }
 } else {
-    console.log("b is big")
+    alert("You are allowed to ride")
 }
